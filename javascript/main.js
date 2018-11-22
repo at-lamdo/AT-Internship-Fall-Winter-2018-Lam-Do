f677636ccaa73ac74e88bd181b9db2d5deb75962
leno <lam.do@asiantech.vn>
2018-11-22 23:17:17 +0700

    Answer for JS question

---
+++ javascript/main.js
@@ -0,0 +1,38 @@
+//Exercise 1
+function sumTwoNumber(a = 0, b = 0) {
+    return a === b ? (a + b) * 3 : a + b;
+}
+//Exercise 2
+function compareWithNineteen(a = 0) {
+    return a > 19 ? (a  - 19) * 3 :  19 - a;
+}
+
+//Exercise 3
+function maskedNumberDivisibleBy3(a = '') {
+var b = [];
+var numberSum = 0 ;
+for (var i = 0 ; i < a.length - 1; i ++) {
+    if (a[i] !== '*') {
+        numberSum += parseInt(a[i]);
+    }
+}
+for (var i = 0 ; i <= 9; i++) {
+    var c = a.replace('*', i);
+    if ((numberSum + i) % 3 === 0) {
+        b.push(c);
+    }
+}
+    return b;
+}
+//Exercise 4
+function maskedNumberDivisibleBy6(a = '') {
+var c = maskedNumber3(a);
+var b = [];
+for (var i = 0 ; i < c.length - 1; i ++) {
+    var st = c[i]; 
+    if (st[st.length - 1] % 2 === 0) {
+        b.push(st);
+    }
+}
+    return b;
+}
